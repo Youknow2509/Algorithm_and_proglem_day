2606e6fc3fcefb2c7a084b6ea159ecdd02acde2a
V-Mac <lytranvinh.2509@gmail.com>
2022-10-30 17:27:16 +0700

    push 30/10/22 - 17:27

--- test/t2.cpp
+++ test/t2.cpp
@@ -1,23 +1,38 @@
-
 #include<bits/stdc++.h>
-
 using namespace std;
 
+class Solution {
+public:
+	long long int NoOfChicks(int n){
+        //code here
+        vector<long long> ck_day;
+        ck_day.push_back(1);
+        long long ck = 1;
+        int j = 0;
+        for(int i = 0; i < n-1 ; i++){
+            if ( i >=5 ){
+                ck -=ck_day[j];
+                j++;
+            }
+
+            ck_day.push_back(ck*2);
+            ck += ck*2;
+            
+        }
+        return ck;
+	} 
+};
+
 int main(){
-    double a[6];
-    cout << "Nhap 6 so vao chuong trinh"<< endl;
-    for( int i = 0; i < 6; i ++) {
-        cout << "A[" << i << "] = " << endl;
-        cin >> a[i];
-    }   
+    int N;
 
-    int c = 0;
-    for( int i = 0; i < 6; i ++) {
-        if ( a[i] > 0) c ++;
-    }
-    cout << "Trong 6 so, co: " << c << "so duong";
-        
+    cout << "Input N = ";
+    cin >> N;
+    cout << endl;
 
-
-    return 0;
+    Solution obj;
+    long long int ans = obj.NoOfChicks(N);
+    cout <<"Output: "<<  ans << endl;
+	
+	return 0;
 }
