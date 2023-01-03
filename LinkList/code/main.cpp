4773b61fac2e40bd59057675ef1df6b63918f390
V-Mac <lytranvinh.2509@gmail.com>
2023-01-03 10:39:39 +0700

    loadding main.cpp ...

--- LinkList/code/main.cpp
+++ LinkList/code/main.cpp
@@ -3,7 +3,35 @@
 
 using namespace std;
 
+struct Node
+{
+    int data;
+    Node *next;
+};
+// creat "a" Node.
+Node *newNode(int data){
+    Node *temp = new Node;
+    temp->data = data;
+    temp->next = NULL;
+    return temp;
+}
+// insert last Node.
+Node *insertlsNode(Node *node, int data){
+    if (node == NULL) node->next = newNode(data);
+    node->next = insertlsNode(node->next, data);
+    return node;
+}
+// insert first Node.
+Node *insertfsNode(Node *node, int data){
+    Node *temp = newNode(data);
+    temp->next = node;
+    return temp;
+}
+
+
 int main(){
+
+
 
     return 0;
 }
