bb27de490da70938b0e117a44ce076dd7290b74d
V-Mac <lytranvinh.2509@gmail.com>
2023-01-04 11:48:18 +0700

    04/01/23 push v_mac main linklist cp

--- LinkList/code/main.cpp
+++ LinkList/code/main.cpp
@@ -8,30 +8,66 @@ struct Node
     int data;
     Node *next;
 };
-// creat "a" Node.
+// Tạo "một" Node.
 Node *newNode(int data){
-    Node *temp = new Node;
+    Node *temp = new Node();
     temp->data = data;
     temp->next = NULL;
     return temp;
 }
-// insert last Node.
+// In linklist.
+void printNode (Node *node){
+    Node *temp = node; // sử dụng temp để không làm thay đổi con trỏ trỏ vào node.
+    while(temp != NULL){
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+}
+// Thêm Node vào "cuối" linklist.
 Node *insertlsNode(Node *node, int data){
-    if (node == NULL) node->next = newNode(data);
-    node->next = insertlsNode(node->next, data);
+    Node *temp = node; // sử dụng temp để không làm thay đổi con trỏ trỏ vào node.
+    if (temp == NULL) {
+        temp = newNode(data);
+        return temp;
+    }
+    temp->next = insertlsNode(temp->next, data);
     return node;
 }
-// insert first Node.
+// Thêm Node vào "đầu" linklist.
 Node *insertfsNode(Node *node, int data){
     Node *temp = newNode(data);
     temp->next = node;
     return temp;
 }
-
+// Thêm Node vào vị trí k bất kì.
+Node *insert_at_k(Node *node, int data,int k){
+    Node *temp_1 = node; // sử dụng temp để không làm thay đổi con trỏ trỏ vào node.
+    // di chuyển con trỏ tới vị trí cần thêm vào.
+    Node *temp_2 = new Node(); // tạo Node chuẩn bị chèn vào vị trí k.
+    temp_2->data = data;
+    for (int i = 0; i < k - 1; i++){
+        temp_1 = temp_1->next; 
+    }
+    temp_2->next = temp_1->next;
+    temp_1->next = temp_2;
+    return node;
+}   
 
 int main(){
 
-
-
+    Node *ll = NULL;
+    ll = insertlsNode(ll, 1);
+    ll = insertlsNode(ll, 2);
+    ll = insertlsNode(ll, 3);
+    printNode(ll); // Output: 1 2 3
+    cout << endl;
+    ll = insertfsNode(ll,4);
+    ll = insertfsNode(ll, 5);
+    printNode(ll); // Output: 5 4 1 2 3
+    cout << endl;
+    ll = insert_at_k(ll, 0, 2);
+    //ll = addAt(ll, 2, 0);
+    printNode(ll); // Output: 5 4 0 1 2 3
+    cout << endl;
     return 0;
 }
