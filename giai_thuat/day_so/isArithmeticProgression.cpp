9795ddb29b99c6ddeb2c93aaf2445f41959cc0eb
V <lytranvinh.2509@gmail.com>
2023-03-19 01:24:43 +0700

    v push isArithmeticProgression.cpp

--- giai_thuat/day_so/isArithmeticProgression.cpp
+++ giai_thuat/day_so/isArithmeticProgression.cpp
@@ -1,3 +1,8 @@
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
 bool isArithmeticProgression(std::vector<int> sequence)
 {
     if (sequence.size() == 1)
@@ -7,4 +12,20 @@ bool isArithmeticProgression(std::vector<int> sequence)
         if (sequence[i]-sequence[i-1]!=k) 
             return false;
     return true;
+}
+
+int main(){
+
+    #ifndef ONLINE_JUDGE
+    freopen("Show_screen/INP.TXT", "r", stdin);
+    freopen("Show_screen/OUT.TXT", "w", stdout);
+    #endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    //
+
+    //
+
+    return 0;
 }
