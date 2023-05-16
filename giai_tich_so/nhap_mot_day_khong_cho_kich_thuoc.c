5cc39550c7e32464ffdcbc9888a3077fb299c423
V <lytranvinh.2509@gmail.com>
2023-05-16 10:19:27 +0700

    update file after

--- giai_tich_so/nhap_mot_day_khong_cho_kich_thuoc.c
+++ giai_tich_so/nhap_mot_day_khong_cho_kich_thuoc.c
@@ -2,23 +2,43 @@
 #include <stdio.h>
 #include<math.h>
 
+void swap(float *a, float *b){
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
 int main(){
 
     FILE *inp_file = fopen("Show_screen/INP.TXT", "r"),
         *out_file = fopen("Show_screen/OUT.TXT", "w");
 
     float sum = 0,
-        temp;
+        temp,
+        arr[10001];
     int count = 0;
 
-    while (!feof(inp_file))
-    {   
+    while (!feof(inp_file)){   
         fscanf(inp_file, "%f", &temp);
+        arr[count] = temp;
         sum += temp;
         count++;
     }
-    
-    fprintf(out_file, "TBC: %f\n", sum/count);
 
+    int median = count/2;
+    
+    for (int i = 0; i < count - 1; i ++){
+        for (int j = i + 1; j < count; j++){
+            if (arr[i] > arr[j]){
+                swap(&arr[i], &arr[j]);
+            }
+        }
+    }
+
+    fprintf(out_file, "Trung binh cong cua day la: %f\n", sum/count);
+    fprintf(out_file, "Trung vi cua day la: %f\n", arr[median]);
+
+    fclose(inp_file);
+    fclose(out_file);
     return 0;
 }
